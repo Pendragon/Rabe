150771bc9a0a63df5389d4d25dc4e2b0e6cd48be
Pendragon <jp@gerst.fr>
2014-05-26 22:39:40 +0200

    add some comment

--- app.js
+++ app.js
@@ -3,15 +3,17 @@
 // Author      : Arthur & Jean-Paul GERST
 // Date        : avril 2014  
 
-var port = 80; // Port de communication ou notre serveur écoute. Par défaut, un serveur web doit être sur le port 80
+var port = 3000; // Port de communication ou notre serveur écoute. Par défaut, un serveur web doit être sur le port 80
 
 var Thermo = require ('./lib/thermo.js');		// Load the Thermo definition & define the hardware id & probe we have
 var thermos = { interieur: new Thermo("28-00000556a548", "Interieur", 10000), exterieur: new Thermo("28-00000555dec3", "Exterieur", 10000) };
+
 var express = require('express');				
 var app = express();
 
 // Monk was brokken for Rasperry PI.
-// I had to recompile it. http://stackoverflow.com/questions/16746134/bus-error-on-mongodb-mongoclient-connect-for-raspberry-pi-arm
+// I had to recompile it. 
+// http://stackoverflow.com/questions/16746134/bus-error-on-mongodb-mongoclient-connect-for-raspberry-pi-arm
 var monk = require('monk'); 
 var db = monk('localhost/belmont');	// Link to the local 'belmont' database. 
 
@@ -48,10 +50,11 @@ app.get('/db', function(req, res) {
 });
 
 // Ajax call to get the various thermo value
+// this only sent the result to the browser. Not an html page. It's intended to be used by a javascipt client side.
 app.get('/thermos/:name', function(req, res) {
 	// the toString() took me an evening to find.
 	// by default res.send will behave differently if the parameter is a string or a number
-	// a string is sent to the browser, and he result from the resquest is 200 (ok), browser will dsiplay the string (html)
+	// a string is sent to the browser to display, and he result from the resquest is 200 (ok), 
 	// a number is considered as the result. As temperature was never 200, the browser consider the request failled...
 	res.send(thermos[req.params.name].value().toString());
 });
