ceab73520504267f84fddc745b93cd0609e1dde2
Pendragon <jp@gerst.fr>
2014-05-26 21:39:57 +0200

    Add the modules

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Rabe
 ====
 
-Rasperry PI Belmont control
+Rasperry PI Belmont control.
+use express Framework, mongodb database
+
+Module Installation 
+===================
+
+npm install express
+npm install monk <= see http://stackoverflow.com/questions/16746134/bus-error-on-mongodb-mongoclient-connect-for-raspberry-pi-arm
+npm install ejs
--- app.js
+++ app.js
@@ -4,20 +4,20 @@
 // Date        : avril 2014  
 
 var port = 80; // Port de communication ou notre serveur écoute. Par défaut, un serveur web doit être sur le port 80
-var version = "0.0.1"; // version de notre programe
 
-var Thermo = require ('./lib/thermo.js');
+var Thermo = require ('./lib/thermo.js');		// Load the Thermo definition & define the hardware id & probe we have
 var thermos = { interieur: new Thermo("28-00000556a548", "Interieur", 10000), exterieur: new Thermo("28-00000555dec3", "Exterieur", 10000) };
-var express = require('express');
+var express = require('express');				
 var app = express();
-//var bodyparser = require('body-parser');
-var monk = require('monk');
-var db = monk('localhost/belmont');
+
+// Monk was brokken for Rasperry PI.
+// I had to recompile it. http://stackoverflow.com/questions/16746134/bus-error-on-mongodb-mongoclient-connect-for-raspberry-pi-arm
+var monk = require('monk'); 
+var db = monk('localhost/belmont');	// Link to the local 'belmont' database. 
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/static'));
-//app.use(bodyparser());
 
 // Mise a jour des variables.
 app.use(function(req, res, next) {
